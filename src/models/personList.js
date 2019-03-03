5c318e283827826d14cc5d0a89951b9b7bdb5371
Kristian Sakarisson <kristian@sakarisson.com>
2019-03-03 13:00:56 +0200

    Append addPerson method to PersonList type

--- src/models/personList.js
+++ src/models/personList.js
@@ -2,8 +2,14 @@ import { types } from 'mobx-state-tree';
 
 import Person from './person';
 
-const PersonList = types.model({
-  persons: types.array(Person),
-});
+const PersonList = types
+  .model({
+    persons: types.array(Person),
+  })
+  .actions(self => ({
+    addPerson(person) {
+      self.persons.push(person);
+    },
+  }));
 
 export default PersonList;
