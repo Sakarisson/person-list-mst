28b1eb482ec6032c563117704febd711b9efcf4c
Kristian Sakarisson <kristian@sakarisson.com>
2019-03-12 16:16:51 +0200

    Add sort people option in Home

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,15 +2,44 @@ import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 import { observer } from 'mobx-react';
 import { compose } from 'recompose';
+import styled from 'styled-components';
 
+import { sortByOptions, sortOrderOptions } from '../../models/personList';
 import generateFriendships from '../../util/generateFriendships';
 import withStore from '../../hoc/withStore';
 import PersonList from '../PersonList';
 import AddPeople from './AddPeople';
+import SortSelect from '../SortSelect';
+
+const SortByContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+
+const SortByText = styled.div`
+  line-height: 20px;
+  font-size: 14px;
+`;
 
 const Home = ({ store }) => (
   <div>
     <AddPeople people={store.people} />
+    <SortByContainer>
+      <SortByText>Sort people by:</SortByText>
+      <SortSelect
+        values={sortByOptions.map(option => ({ key: option, name: option }))}
+        selected={store.sortBy}
+        onChange={e => store.setSortBy(e.target.value)}
+      />
+    </SortByContainer>
+    <SortByContainer>
+      <SortByText>Sort people by:</SortByText>
+      <SortSelect
+        values={sortOrderOptions.map(option => ({ key: option, name: option }))}
+        selected={store.sortOrder}
+        onChange={e => store.setSortOrder(e.target.value)}
+      />
+    </SortByContainer>
     {store.people.length > 1 && (
       <button type="button" onClick={() => generateFriendships(store, 5)}>
         Generate friendships
